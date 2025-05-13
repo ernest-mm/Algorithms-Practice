25c35032141211d312763c8421171d0e77e302c8
Ernest M. <180271155+ernest-mm@users.noreply.github.com>
2025-05-13 19:14:52 +0200

    Create 49-Group-Anagrams.py

---
+++ Leetcode/49-Group-Anagrams.py
@@ -0,0 +1,63 @@
+"""
+Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+
+ 
+
+Example 1:
+
+Input: strs = ["eat","tea","tan","ate","nat","bat"]
+
+Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+
+Explanation:
+
+There is no string in strs that can be rearranged to form "bat".
+The strings "nat" and "tan" are anagrams as they can be rearranged to form each other.
+The strings "ate", "eat", and "tea" are anagrams as they can be rearranged to form each other.
+Example 2:
+
+Input: strs = [""]
+
+Output: [[""]]
+
+Example 3:
+
+Input: strs = ["a"]
+
+Output: [["a"]]
+
+ 
+
+Constraints:
+
+1 <= strs.length <= 104
+0 <= strs[i].length <= 100
+strs[i] consists of lowercase English letters.
+"""
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        sorted_strs = []
+
+        for i in range(len(strs)):
+            sorted_chars = sorted(strs[i])
+            sorted_str = ''.join(sorted_chars)
+            sorted_strs.append((sorted_str, i))
+
+        anagram_group = {}
+
+        for sorted_str, i in sorted_strs:
+            anagram_group[sorted_str] = []
+
+        for sorted_str, i in sorted_strs:
+            temp = anagram_group[sorted_str]
+            temp.append(strs[i])
+            anagram_group[sorted_str] = temp
+
+        result = []
+
+        for group in anagram_group:
+            result.append(anagram_group[group])
+
+        return result
+        
