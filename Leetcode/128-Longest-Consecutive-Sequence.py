cc32eca5558d2a89843e43002d3d092f8e38bfe0
Ernest M. <180271155+ernest-mm@users.noreply.github.com>
2025-05-22 22:30:47 +0200

    Added the solution for 128-Longest-Consecutive-Sequence.py

--- Leetcode/128-Longest-Consecutive-Sequence.py
+++ Leetcode/128-Longest-Consecutive-Sequence.py
@@ -25,3 +25,19 @@ Constraints:
 0 <= nums.length <= 10^5
 -10^9 <= nums[i] <= 10^9
 """
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        nums_set = set(nums)
+        sequences_dict = {}
+        longest = 0
+
+        for num in nums_set:
+            sequence_length_1 = sequences_dict.get(num - 1, 0)
+            sequence_length_2 = sequences_dict.get(num + 1, 0)
+            
+            temp_longest = sequence_length_1 + sequence_length_2 + 1
+            sequences_dict[num - sequence_length_1] = temp_longest
+            sequences_dict[num + sequence_length_2] = temp_longest
+            longest = max(temp_longest, longest)
+        
+        return longest
